01ff3e52adb040b746202025e4894fd33045cead
KavyaNayani <kavyanayani05@gmail.com>
2024-08-20 23:41:01 -0500

    goroutines

---
+++ ass1/sum.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sum(arr []int, ch chan int) {
+	total := 0
+	for _, value := range arr {
+		total += value
+	}
+	ch <- total
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+	ch := make(chan int, 4)
+	size := 3
+	for i := 0; i < len(arr)/size; i++ {
+		start := i * size
+		end := start + size
+		go sum(arr[start:end], ch)
+	}
+	total := 0
+	for i := 0; i < len(arr)/size; i++ {
+		partialSum := <-ch
+		total += partialSum
+	}
+	fmt.Printf("The total sum is: %d\n", total)
+}
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+/*package main
 
 import (
 	"fmt"
@@ -20,5 +20,61 @@ func main() {
 		fmt.Println("i value is", i)
 		i++
 	}
+	//arrays
+	arr := [4]int{2, 3, 4, 5}
+	arr[2] = 11
+	fmt.Println("Array is", arr)
+	fmt.Println("Length of an array is", len(arr))
+	//slices
+	var names = []string{"apple", "mango", "peach"}
+	names[1] = "strawberry"
+	names = append(names, "kiwi")
+	fmt.Println("After appending", names)
+	var new_arr = arr[1:3]
+	fmt.Println("New array is", new_arr)
+	//maps
+	scores := map[string]float64{
+		"Emma":   89.5,
+		"Ashley": 90.2,
+		"Robert": 78.4,
+		"Peter":  92.3,
+	}
+	fmt.Println(scores)
+	scores["Robert"] = 80.0
+	fmt.Println(scores)
+	delete(scores, "Emma")
+	for k, v := range scores {
+		fmt.Println(k, "-", v)
+	}
+}
+*/
 
+package main
+
+import "fmt"
+
+func primes(n int, ch chan int) {
+	for i := 2; i <= n; i++ {
+		isPrime := true
+		for p := 2; p*p <= i; p++ {
+			if i%p == 0 {
+				isPrime = false
+				break
+			}
+		}
+		if isPrime {
+			ch <- i
+		}
+	}
+	close(ch)
+}
+
+func main() {
+	ch := make(chan int)
+	go primes(50, ch)
+	//x := len(ch)
+	//fmt.Println(x)
+	for value := range ch {
+		fmt.Println(value)
+	}
 }
